0680b309b9eab627e35ff0cdaba1635e075a0b43
TJMaxx27 <99042166+TJMaxx27@users.noreply.github.com>
2022-04-25 00:13:13 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from extensions import ConvertionException, CryptoConverter
 
 bot = telebot.TeleBot(TOKEN)
 
-
 @bot.message_handler(commands=['start', 'help'])
 def instruction(message: telebot.types.Message):
     text = 'Привет! Я бот - конвертер валют. \n \
@@ -16,15 +15,12 @@ def instruction(message: telebot.types.Message):
 Напомнить формат сообщения для расчета курса валют: /help'
     bot.reply_to(message, text)
 
-
-
 @bot.message_handler(commands=['values'])
 def values(message: telebot.types.Message):
     text = 'Доступные валюты:'
     for key in keys.keys():
         text = '\n'.join((text, key,))
     bot.reply_to(message, text)
-
 
 @bot.message_handler(content_types=['text'])
 def get_price(message: telebot.types.Message):
@@ -47,30 +43,4 @@ def get_price(message: telebot.types.Message):
         text = f'Цена {amount} {quote} в {base} = {total_base}'
         bot.send_message(message.chat.id, text)
 
-
 bot.polling()
-
-
-
-
-# import redis
-# import json
-#
-# red = redis.Redis (
-#     host='redis-19621.c250.eu-central-1-1.ec2.cloud.redislabs.com',
-#     port=19621,
-#     password='nMoTPNI1YR4WqwWHMakrHU5QV5QjoXB8'
-# )
-#
-# red.delete('dict1') # удаляются ключи с помощью метода .delete()
-# print(red.get('dict1'))
-
-
-
-
-
-
-
-
-
-
